931246f8cd342c468f0694031bba559bc3b09a96
Stevensis <informatica@fkinal.edu>
2016-05-27 11:11:06 -0600

    Mensaje

--- README.md
+++
@@ -1,3 +0,0 @@
-# Laboratorio-1
-Cuantas tazas de café se te antojaron hoy? 
->>>>>>> 2f59658a7e9a01bd9068088aeaa20dc1481ef456
--- android-control-pers-3/app/src/main/java/net/sgoliver/android/controlpers3/OnCasillaSeleccionadaListener.java
+++
@@ -1,6 +0,0 @@
-package net.sgoliver.android.controlpers3;
-
-public interface OnCasillaSeleccionadaListener
-{
-    void onCasillaSeleccionada(int fila, int columna);
-}
